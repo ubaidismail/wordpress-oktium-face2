5431ce3159402a2509ffabbf173bb6ad7cefce22
Rahil Wazir <rahilwazirali@gmail.com>
2021-12-21 13:17:53 +0000

    Update face2-widget.php

--- face2-widget.php
+++ face2-widget.php
@@ -14,11 +14,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Oktium Face2 widget
- * Plugin URI:        https://kb.oktium.com/
+ * Plugin URI:        https://oktium.zendesk.com/hc/en-us/articles/360023346574-Face2-Wordpress-plugin
  * Description:       This is a plugin for implementing Face2 button as floating widget.
  * Version:           1.0.0
  * Author:            oktium, qfnetwork, ubaid, rahilwazir
- * Author URI:        https://kb.oktium.com/
+ * Author URI:        https://oktium.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       face2-widget
